32056ad459c6803214c35d3d0d28c03e9b85bcac
Roy Segall <roy.segall@gizra.com>
2016-01-03 17:31:32 +0200

    Use OG membership for users only.

--- src/Plugin/Field/FieldType/OgMembershipReferenceItem.php
+++ src/Plugin/Field/FieldType/OgMembershipReferenceItem.php
@@ -23,46 +23,8 @@ use Drupal\Core\Form\FormStateInterface;
  *   default_widget = "og_complex",
  *   default_formatter = "og_complex",
  *   list_class = "\Drupal\og\Plugin\Field\FieldType\OgMembershipReferenceItemList",
- *   constraints = {"ValidReference" = {}, "ValidOgMembershipReference" = {}}
  * )
  */
 class OgMembershipReferenceItem extends EntityReferenceItem {
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultFieldSettings() {
-    $settings = parent::defaultFieldSettings();
-    $settings['access_override'] = FALSE;
-
-    return $settings;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
-    $form = parent::fieldSettingsForm($form, $form_state);
-
-    // Field access settings.
-    $form['access_override'] = [
-      '#title' => $this->t('Allow entity access to control field access'),
-      '#description' => $this->t('By default, the <em>administer group</em> permission is required to directly edit this field. Selecting this option will allow access to anybody with access to edit the entity.'),
-      '#type' => 'checkbox',
-      '#default_value' => $this->getSetting('access_override'),
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function schema(FieldStorageDefinitionInterface $field_definition) {
-    // @todo When the FieldStorageConfig::hasCustomStorage method can be changed
-    // this will not be needed to prevent errors. Can just be an empty array,
-    // similar to PathItem.
-    return ['columns' => []];
-  }
 
 }
