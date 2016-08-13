d695f45e60962ac250ec735162e55e9d8e576026
golddragon007 <mdanko@brainsum.com>
2016-08-13 15:49:49 +0200

    move Og::getRole() to OgRole::loadByGroupTypeGroupBundleAndName()

--- tests/src/Functional/GroupSubscribeTest.php
+++ tests/src/Functional/GroupSubscribeTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\og\Functional;
 use Drupal\Component\Utility\Unicode;
 use Drupal\node\Entity\Node;
 use Drupal\og\Entity\OgMembershipType;
+use Drupal\og\Entity\OgRole;
 use Drupal\og\Og;
 use Drupal\og\OgMembershipInterface;
 use Drupal\og\OgRoleInterface;
@@ -136,8 +137,7 @@ class GroupSubscribeTest extends BrowserTestBase {
     ]);
     $this->group4->save();
 
-    /** @var \Drupal\og\Entity\OgRole $role */
-    $role = Og::getRole('node', $this->groupBundle1, OgRoleInterface::ANONYMOUS);
+    $role = OgRole::loadByGroupTypeGroupBundleAndName('node', $this->groupBundle1, OgRoleInterface::ANONYMOUS);
     $role
       ->grantPermission('subscribe without approval')
       ->save();
